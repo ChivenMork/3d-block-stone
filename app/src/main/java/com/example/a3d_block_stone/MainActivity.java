4146fa210281ab515811af218f480ff51e9ef5aa
MORK Chiven <mork.chiven@optistech.com>
2021-07-03 09:12:28 +0700

    erroe

--- app/src/main/java/com/example/a3d_block_stone/MainActivity.java
+++ app/src/main/java/com/example/a3d_block_stone/MainActivity.java
@@ -1,14 +1,38 @@
 package com.example.a3d_block_stone;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModel;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewTreeObserver;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        final View content =findViewById(android.R.id.content);
+        content.getViewTreeObserver().addOnPreDrawListener(
+                new ViewTreeObserver.OnPreDrawListener() {
+                         @Override
+                    public boolean onPreDraw() {
+                        // Check if the initial data is ready.
+                        if (mViewModel.isReady()) {
+                            // The content is ready; start drawing.
+                            content.getViewTreeObserver().removeOnPreDrawListener(this);
+                            return true;
+                        } else {
+                            // The content is not ready; suspend.
+                            return false;
+                        }
+                    }
+                });
+        }
+
+
     }
-}
